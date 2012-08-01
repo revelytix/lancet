f178f1fde6384d4a256dd7b90750f1aeada9e617
Phil Hagelberg <technomancy@gmail.com>
2012-08-01 12:10:36 -0700

    Release 1.0.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lancet "1.0.2-SNAPSHOT"
+(defproject lancet "1.0.2"
   :description "Dependency-based builds, Clojure Style, with optional Ant underneath."
   :license {:name "MIT X11 License"}
   :dependencies [[org.apache.ant/ant "1.8.4"]]
